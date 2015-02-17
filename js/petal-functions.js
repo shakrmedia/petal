671df11aa76603c03959320db150b9dc0d81b207
Minku Lee <premist@me.com>
2015-02-17 16:55:11 +0900

    pluginify javascript

--- js/petal-functions.js
+++ js/petal-functions.js
@@ -1,54 +1,57 @@
 /* petal-functions.js for Petal */
-$(document).ready(function() {
-
-
-	// toggle button
-	$('[data-toggle="toggle"]').click(function() {
-		var self = $(this);
-
-		if (self.is('.disabled')) {
-			return;
-		}
-
-		if (self.attr('data-toggle-state') === 'active') {
-			self.removeClass('active').removeAttr('data-toggle-state');
-		} else {
-			self.addClass('active').attr('data-toggle-state', 'active');
-		}
-
-	});
-
-
-	// dropdown toggle
-	function clearMenus() {
-		$('[data-toggle="dropdown"]').parent().removeClass('active').removeAttr('data-dropdown-state');
+;(function ($, window) {
+	"use strict";
+	
+	function clearMenus(self) {
+		self.parent().removeClass('active').removeAttr('data-dropdown-state');
 	}
-
-	$('[data-toggle="dropdown"]').click(function() {
-		var self = $(this);
-
-		if (self.is('.disabled')) {
-			return;
-		}
-
-		var dropdown_state = self.parent().attr('data-dropdown-state');
-		
-		clearMenus();
-		
-		if (!dropdown_state) {
-			self.parent().addClass('active').attr('data-dropdown-state','open');
-		}		
-	});
 
 	$(document).on('click', function(event) {
 		var target = $(event.target);
 
-		if(target.parent().hasClass('submenu') || target.attr('data-toggle') === 'dropdown') { return; }
+		if(target.parent().hasClass('submenu') || target.attr('data-toggle') === 'dropdown') return;
 
-		clearMenus();
+		clearMenus(target);
 
 	});
-
-});
-
-
+	
+	// toggle button
+	$.fn.toggle = function() {
+		$(this)
+			.prop('data-toggle','toggle')
+			.click(function() {
+				var self = $(this);
+		
+				if (self.is('.disabled')) return;
+		
+				if (self.attr('data-toggle-state') === 'active') {
+					self.removeClass('active').removeAttr('data-toggle-state');
+				} else {
+					self.addClass('active').attr('data-toggle-state', 'active');
+				}
+		
+			});
+	}
+	
+	// dropdown toggle
+	$.fn.dropdown = function() {
+		$(this)
+			.prop('data-toggle','dropdown')
+			.click(function() {
+				var self = $(this);
+		
+				if (self.is('.disabled')) return;
+		
+				var dropdown_state = self.parent().attr('data-dropdown-state');
+				
+				clearMenus(self);
+				
+				if (!dropdown_state) {
+					self.parent().addClass('active').attr('data-dropdown-state','open');
+				}		
+			});
+	};
+	
+	$('[data-toggle="toggle"]').toggle();
+	$('[data-toggle="dropdown"]').dropdown();
+})(jQuery, window);
